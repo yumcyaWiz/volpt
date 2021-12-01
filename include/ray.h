ba1fa4ade2db93b11d866e00d46c0d7a5bfdda0e
yumcyawiz <hashdolf1923@gmail.com>
2021-12-02 02:49:48 +0900

    add medium on ray

---
+++ include/ray.h
@@ -0,0 +1,37 @@
+#ifndef _RAY_H
+#define _RAY_H
+#include <stack>
+
+#include "core.h"
+
+// forward declaration
+class Medium;
+
+class Ray {
+ private:
+  std::stack<const Medium*> mediums;
+
+ public:
+  Vec3f origin;
+  Vec3f direction;
+  static constexpr float tmin = RAY_EPS;
+  mutable float tmax = std::numeric_limits<float>::max();
+
+  Ray() {}
+  Ray(const Vec3f& origin, const Vec3f& direction)
+      : origin(origin), direction(direction) {}
+
+  Vec3f operator()(float t) const { return origin + t * direction; }
+
+  const Medium* getCurrentMedium() const { return mediums.top(); }
+
+  void pushMedium(const Medium* medium) { mediums.push(medium); }
+
+  const Medium* popMedium() {
+    const Medium* ret = mediums.top();
+    mediums.pop();
+    return ret;
+  }
+};
+
+#endif
