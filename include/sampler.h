3d463087dea51302108220d2f375044d2ab5f932
yumcyawiz <hashdolf1923@gmail.com>
2021-12-05 17:30:02 +0900

    impl DiscreteEmpiricalDistribution1D

--- include/sampler.h
+++ include/sampler.h
@@ -98,4 +98,43 @@ Vec2f sampleDisk(const Vec2f& uv, float R, float& pdf) {
   return Vec2f(r * std::cos(theta), r * std::sin(theta));
 }
 
+// sample value from 1D discrete empirical distribution
+class DiscreteEmpiricalDistribution1D {
+ private:
+  std::vector<float> cdf;
+
+ public:
+  DiscreteEmpiricalDistribution1D(const float* values, unsigned int N) {
+    // sum f
+    float sum = 0;
+    for (std::size_t i = 0; i < N; ++i) {
+      sum += values[i];
+    }
+
+    // compute cdf
+    cdf.resize(N + 1);
+    cdf[0] = 0;
+    for (std::size_t i = 1; i < N + 1; ++i) {
+      cdf[i] = cdf[i - 1] + values[i - 1] / sum;
+    }
+  }
+
+  DiscreteEmpiricalDistribution1D(const std::vector<float>& values)
+      : DiscreteEmpiricalDistribution1D(values.data(), values.size()) {}
+
+  uint32_t sample(float u, float& pdf) const {
+    // inverse cdf
+    int x = std::lower_bound(cdf.begin(), cdf.end(), u) - cdf.begin();
+    if (x == 0) {
+      x++;
+    }
+
+    // compute pdf
+    pdf = cdf[x] - cdf[x - 1];
+
+    // NOTE: cdf's index is +1 from values
+    return x - 1;
+  }
+};
+
 #endif
