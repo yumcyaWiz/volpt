d922095c2b1b7948ca1434128ed2e465c9faf57e
yumcyawiz <hashdolf1923@gmail.com>
2021-12-05 15:46:27 +0900

    add test scene

---
+++ examples/sakana-test.cpp
@@ -0,0 +1,38 @@
+#include "camera.h"
+#include "integrator.h"
+#include "medium.h"
+#include "scene.h"
+
+int main() {
+  const uint32_t width = 512;
+  const uint32_t height = 512;
+  const uint32_t n_samples = 10000;
+  const uint32_t max_depth = 10000;
+
+  Image image(width, height);
+
+  const Vec3f camera_pos = Vec3f(0, 10, 162.6);
+  const Vec3f camera_forward = normalize(Vec3f(0, -0.042612, -1));
+  const float FOV = 0.25f * PI;
+
+  const auto camera =
+      std::make_shared<PinholeCamera>(camera_pos, camera_forward, FOV);
+
+  // build scene
+  Scene scene;
+  scene.loadModel("sakana-test.obj");
+  scene.build();
+
+  // render
+  UniformSampler sampler;
+  PathTracing integrator(camera, n_samples, max_depth);
+  integrator.render(scene, sampler, image);
+
+  // gamma correction
+  image.gammaCorrection(2.2f);
+
+  // output image
+  image.writePPM("output.ppm");
+
+  return 0;
+}
