b8e1e78fcf6a505d8122367681a2785ccbef9572
yumcyawiz <hashdolf1923@gmail.com>
2021-12-12 10:40:11 +0900

    move tests

--- tests/test_hetero.cpp
+++ tests/test_hetero.cpp
@@ -11,10 +11,10 @@ int main() {
 
   Image image(width, height);
 
-  // const Vec3f camera_pos = 1.5 * Vec3f(48, 24, 24);
-  // const Vec3f camera_lookat = Vec3f(0, 12, 0);
-  const Vec3f camera_pos = 30 * Vec3f(0, 1.5, 6);
-  const Vec3f camera_lookat = 30 * Vec3f(0, 1.5, 0);
+  const Vec3f camera_pos = 1.5 * Vec3f(48, 24, 24);
+  const Vec3f camera_lookat = Vec3f(0, 12, 0);
+  // const Vec3f camera_pos = 30 * Vec3f(0, 1.5, 6);
+  // const Vec3f camera_lookat = 30 * Vec3f(0, 1.5, 0);
   const Vec3f camera_forward = normalize(camera_lookat - camera_pos);
   const float FOV = 0.25 * PI;
 
@@ -23,12 +23,12 @@ int main() {
 
   // build scene
   Scene scene;
-  // scene.loadObj("smoke_test.obj");
-  // scene.loadVDB("smoke2.vdb", 0, Vec3f(0.8f, 0.01f, 0.2f),
-  //               Vec3f(1.0f, 1.0f, 1.0f), 20.0f);
+  scene.loadObj("smoke_test.obj");
+  scene.loadVDB("smoke2.vdb", 0, Vec3f(0.1f, 0.8f, 0.2f),
+                Vec3f(1.0f, 1.0f, 1.0f), 40.0f);
 
-  scene.loadObj("cornellbox_smoke.obj");
-  scene.loadVDB("smoke.vdb", 0, Vec3f(0.1f), Vec3f(1.0f), 5.0f);
+  // scene.loadObj("cornellbox_smoke.obj");
+  // scene.loadVDB("smoke.vdb", 0, Vec3f(0.1f), Vec3f(1.0f), 5.0f);
   scene.build();
 
   // render
